04d2f644e73c27b42516e019448741e6fc171f06
ilyasbounoua <bounoua.ilyas@outlook.com>
2025-11-18 12:02:24 +0100

    Add JPA metamodel classes for Interaction, Module, Sound
    Generated JPA static metamodel classes for Interaction, Module, and Sound entities to support type-safe criteria queries. Includes corresponding .class and .java files in the target directories.

---
+++ back/PEPs_back/target/generated-sources/annotations/peps/peps_back/items/Module_.java
@@ -0,0 +1,24 @@
+package peps.peps_back.items;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.CollectionAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import peps.peps_back.items.Interaction;
+
+@Generated(value="EclipseLink-2.7.15.v20240516-rNA", date="2025-11-18T12:00:21")
+@StaticMetamodel(Module.class)
+public class Module_ { 
+
+    public static volatile SingularAttribute<Module, Integer> volume;
+    public static volatile SingularAttribute<Module, Integer> idmodule;
+    public static volatile SingularAttribute<Module, Date> lastSeen;
+    public static volatile SingularAttribute<Module, Boolean> actif;
+    public static volatile SingularAttribute<Module, String> ipAdress;
+    public static volatile SingularAttribute<Module, String> nom;
+    public static volatile CollectionAttribute<Module, Interaction> interactionCollection;
+    public static volatile SingularAttribute<Module, String> status;
+    public static volatile SingularAttribute<Module, String> currentMode;
+
+}
