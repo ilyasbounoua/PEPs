60452f7f1133e5426f0f17f9aabf99898ca17f54
momodia0102 <m2000dia@gmail.com>
2026-02-17 15:37:34 +0100

    test: ajout de provideHttpClient pour SoundAddComponent

--- front/pepsfront/src/app/components/sounds/sound-add/sound-add.spec.ts
+++ front/pepsfront/src/app/components/sounds/sound-add/sound-add.spec.ts
@@ -1,22 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-// 1. On importe le bon nom de classe
-import { SoundAddComponent } from './sound-add'; 
+import { provideHttpClient } from '@angular/common/http'; 
+import { provideHttpClientTesting } from '@angular/common/http/testing'; 
+import { SoundAddComponent } from './sound-add';
 
 describe('SoundAddComponent', () => {
-  // 2. On met à jour les types ici
   let component: SoundAddComponent;
   let fixture: ComponentFixture<SoundAddComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      // 3. On met à jour l'import du module standalone
-      imports: [SoundAddComponent] 
+      imports: [SoundAddComponent],
+      providers: [
+        provideHttpClient(), // 3. On fournit le HttpClient
+        provideHttpClientTesting() // 4. On fournit l'outil de test associé
+      ]
     })
     .compileComponents();
 
-    // 4. On crée le composant avec la bonne classe
-    fixture = TestBed.createComponent(SoundAddComponent); 
+    fixture = TestBed.createComponent(SoundAddComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
